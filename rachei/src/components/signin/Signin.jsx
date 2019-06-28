cc6e8fec6aa15cc187ae04bd499a80127a59d67e
Paul Divet <pauldivet@MacBook-Pro-de-Paul.local>
2019-06-27 21:40:08 -0300

    Home criada e routes para Sign up e Sign in

---
+++ rachei/src/components/signin/Signin.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Offheader from "../off-header/Offheader";
+import "./signin.css";
+
+class Signin extends Component {
+  render() {
+    return (
+      <div>
+        <Offheader />
+        <p>THIS IS THE SIGNIN PAGE</p>
+      </div>
+    );
+  }
+}
+
+export default Signin;
